ca71d5c699a9ab2a6e0b6c4bac41b4081ab1b2be
Karen.Hsieh <karen.hsieh@N2109002-MacBook-Pro.local>
2024-12-09 07:33:03 +0800

    client.py server.py

---
+++ client.py
@@ -0,0 +1,89 @@
+import socket
+from os.path import isfile, getsize
+
+
+conn_ip = "127.0.0.1"
+conn_port = 8888
+1
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP
+client_socket.connect((conn_ip, conn_port))
+
+
+def receive_message(conn):
+    # Keep reading until the delimiter is found
+    try:
+        message = b""
+        first_chunk = conn.recv(4096)
+        if "[TABLE]".encode('utf-8') not in first_chunk:
+            return first_chunk.decode('utf-8')
+        
+        message += first_chunk
+
+        while True:
+            chunk = conn.recv(4096)
+            if not chunk:
+                raise ConnectionError("Connection lost while receiving data")
+            message += chunk
+            if "[END]".encode('utf-8') in message:
+                break
+        return message.decode('utf-8').replace("[END]", '').replace("[TABLE]", '')
+    except Exception:
+        print("Receive message error.")
+        return
+        # client_socket.close()
+
+try: 
+    while True: # Keep receiving and sending message with server
+        
+        recv_msg = receive_message(client_socket)
+        # print(f'recv_msg: {recv_msg}')
+        # client_socket.recv(10000).decode('utf-8')
+        if not recv_msg:
+            print("Connection closed by the server.")
+            break
+        if recv_msg.find("[EXIT]") != -1:
+            print(recv_msg.replace("[EXIT]", ''), end='')
+            break
+        if recv_msg.find("[CSV]") != -1:
+            print(recv_msg.replace("[CSV]", '').replace("[INPUT]", ''), end='')
+            
+            filename = input().strip()
+            if not isfile(filename) or filename.find(".csv") == -1:
+                print(f'File \'{filename}\' is not found or is not a csv file.')
+                client_socket.send("[NOTFOUND]".encode('utf-8'))
+            
+
+            
+            file_size = getsize(filename)
+            # print(f'File \'{filename}\' found, file_size = {file_size}')
+            client_socket.send(str(file_size).encode())
+        
+            # Wait for server response
+            client_socket.recv(1024)
+            print(f'Start transferring file...')
+
+            with open(filename, 'rb') as f:
+                while (chunk := f.read(1024)):
+                    client_socket.send(chunk)
+            print(f'File \'{filename}\' sent successfully.')
+        
+        
+        elif recv_msg.find("[INPUT]") != -1:
+            print(recv_msg.replace("[INPUT]", ''), end='')
+
+            send_msg = input().strip()
+            while len(send_msg) == 0:
+                print("Input cannot be empty. Please enter again:", end=' ')
+                send_msg = input().strip()
+
+            if send_msg == "exit":
+                break            
+            client_socket.send(send_msg.encode('utf-8'))
+        
+
+        else:
+            print(recv_msg, end='')
+        
+finally:
+    print("Connection close.")
+    client_socket.close()
---
+++ server.py
@@ -0,0 +1,89 @@
+import socket
+import traceback
+from threading import Thread
+
+from db_utils import *
+from action_handler import *
+from helper import *
+
+
+welcome_action = ['farmer_signup', 'consumer_signup', 'farmer_login', 'consumer_login', 'exit']  
+
+
+def handle_connection(conn, client_addr):
+    try:
+        
+        while True: # Welcome Page
+            conn.send("----------------------------------------\nWelcome to Farmly!\n".encode('utf-8'))
+            conn.send(f'[INPUT]Please select your action:\n{list_option(welcome_action)}---> '.encode('utf-8'))
+            action_str = get_selection(conn, welcome_action)
+
+
+            # 獲取函數並執行
+            if action_str in globals():
+                func_to_call = globals()[action_str]
+                if callable(func_to_call):
+                    user = func_to_call(conn)
+                else:
+                    print(f"{action_str} is not callable.")
+            else:
+                print(f"{action_str} does not exist.")
+
+            print("return to server")
+            print(user.get_username())
+            if user == -1:
+                raise Exception("End connection")
+            
+            send_msg =  f'\n----------------------------------------\n\nHi {user.get_username()}!\n' + \
+                        f'[ User Info ] {user.get_info_msg_no_pwd()}\n'
+            conn.send(send_msg.encode('utf-8'))
+
+            while True: # Function Page
+                
+                conn.send(f'\n----------------------------------------\n\n'.encode('utf-8'))
+                actions = user.get_available_action()
+                conn.send(f'[INPUT]Please select your option:\n{list_option(actions)}---> '.encode('utf-8'))
+                action_atr = get_selection(conn, actions)
+                func_to_call = user.get_function_name(action_atr)
+                ret = func_to_call(conn, user)
+
+                if ret == -1:
+                    break
+
+    except Exception as e:
+        print(f"Connection with {client_addr} close.")
+        print(f"Error occurred: {str(e)}")
+        traceback.print_exc()  # 輸出完整的錯誤追蹤
+        conn.close()
+    finally:
+        print(f"Connection with {client_addr} close.")
+        conn.close()
+
+
+
+if __name__ == '__main__':
+
+    
+    db = db_connect()
+    cur = db.cursor()
+
+    bind_ip = "127.0.0.1"
+    bind_port = 8888
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((bind_ip, bind_port))
+    server_socket.listen(5)
+
+    print(f'Server listening on {bind_ip}:{bind_port} ...')
+
+
+    try:
+        while True:
+            (conn, client_addr) = server_socket.accept()
+            print("Connect to client:", client_addr)
+
+            thread = Thread(target=handle_connection, args=(conn, client_addr))
+            thread.start()
+    finally:
+        db.close()
+        server_socket.close()
