8cb43fbea90dad2a98ccf6f6e14223b95990872c
Karen.Hsieh <karen.hsieh@N2109002-MacBook-Pro.local>
2024-12-09 01:58:05 +0800

    modify Role.py

--- role/Role.py
+++ role/Role.py
@@ -11,15 +11,21 @@ class Role():
 
     def get_username(self):
         return self.name
+    
     def get_userid(self):
         return self.userid
+    
     def get_email(self):
         return self.email
+    
     def get_available_action(self):
         return list(self.user_action.keys())
+    
     def get_function_name(self, action_str):
         return self.user_action['action_str']
+    
     def get_info_msg_no_pwd(self):
         return f'userid: {self.userid}, username: {self.name}, phone_number: {self.phone_number}, email: {self.email}, role: {type(self).__name__}'
+    
     def get_info_msg(self):
         return f'userid: {self.userid}, username: {self.name}, pwd: {self.password}, phone_number: {self.phone_number}, email: {self.email}, role: {type(self).__name__}'
